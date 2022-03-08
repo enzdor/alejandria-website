a7895963823644733d324ef086cb7c397a666e9e
enzdor <enzo01lb@gmail.com>
2022-03-08 02:09:36 -0300

    starting to use react router

--- alejandria/src/App.js
+++ alejandria/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './Pages/Home';
+import Books from './Pages/Books';
 
 function App() {
   return (
     <Router>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+      </Routes>
     </Router>
   );
 }
