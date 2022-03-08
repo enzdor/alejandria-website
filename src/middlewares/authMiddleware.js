99c2ea890b9cb133726eb642e8286893a6cf8da7
enzdor <enzo01lb@gmail.com>
2022-03-07 23:51:02 -0300

    delete old things

--- src/middlewares/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-function authMiddleware(req, res, next){
-    if(req.session.userLogged === undefined){
-        res.render('register')
-    } else {
-        next()
-    }
-}
-
-module.exports = authMiddleware;
