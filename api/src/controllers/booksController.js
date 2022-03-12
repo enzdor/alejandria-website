9191b8fe31d709511e8b7507bad1f0b7da436738
enzdor <enzo01lb@gmail.com>
2022-03-11 22:57:39 -0300

    user can see favourite and created books in profile but some bugs

--- api/src/controllers/booksController.js
+++ api/src/controllers/booksController.js
@@ -1,4 +1,6 @@
 const db = require('../database/models')
+const { Op } = require("sequelize");
+
 module.exports = {
     list: async (req, res) => {
         const books = await db.Book.findAll({include: {all: true}})
@@ -38,11 +40,84 @@ module.exports = {
                 total: books.length,
                 url: "api/books",
             },
-            data: books
+            data: arrayBooks
         }
 
         res.json(response)
         
+    },
+    listCreatedSub: async (req, res) => {
+        const books = await db.Book.findAll({
+            where: {user_sub: req.params.user_sub},
+            include: {all: true}
+        })
+        const arrayBooks = []
+
+        for (const book of books) {
+            arrayBooks.push(book.dataValues)
+        }
+
+        for (const book of arrayBooks) {
+            book.isFavourite = false
+            for (let fav of book.favourite) {
+                if(fav.user_sub === req.params.user_sub){
+                    book.isFavourite = true
+                }
+            }
+        }
+
+        const response = {
+            meta: {
+                status: 200,
+                total: books.length,
+                url: "api/books",
+            },
+            data: arrayBooks
+        }
+
+        res.json(response)
+    },
+    listFavouriteSub: async (req, res) => {
+        const allBooks = await db.Book.findAll({include: {all: true}})
+        const arrayBooks = []
+        const finalBooks = []
+
+        for (const book of allBooks) {
+            for (let fav of book.favourite) {
+                if(fav.user_sub === req.params.user_sub){
+                    arrayBooks.push(book.id)
+                }
+            }
+        }
+
+        const books = await db.Book.findAll({
+            include: {all: true},
+            where: {id: {[Op.or]: arrayBooks}}
+        })
+
+        for (const book of books) {
+            finalBooks.push(book.dataValues)
+        }
+
+        for (const book of finalBooks) {
+            book.isFavourite = false
+            for (let fav of book.favourite) {
+                if(fav.user_sub === req.params.user_sub){
+                    book.isFavourite = true
+                }
+            }
+        }
+
+        const response = {
+            meta: {
+                status: 200,
+                total: books.length,
+                url: "api/books",
+            },
+            data: books
+        }
+
+        res.json(response)
     },
     detail: async (req, res) => {
         const book = await db.Book.findByPk(req.params.id,
@@ -70,7 +145,6 @@ module.exports = {
             user_sub: req.body.user_id
         })
 
-        console.log('3');
         res.send('done')
     }
 }
--- api/src/routers/booksRouter.js
+++ api/src/routers/booksRouter.js
@@ -6,6 +6,8 @@ const booksController = require('../controllers/booksController');
 router.get('/books', booksController.list)
 router.get('/books/:id', booksController.detail)
 router.get('/books/logged/:user_sub', booksController.listUserSub)
+router.get('/books/created/:user_sub', booksController.listCreatedSub)
+router.get('/books/favourite/:user_sub', booksController.listFavouriteSub)
 router.post('/books', booksController.create)
 
 module.exports = router
