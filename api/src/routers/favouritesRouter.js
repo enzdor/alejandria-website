b1d9a559262b342574ca3de611952a734c57ecae
enzdor <enzo01lb@gmail.com>
2022-03-31 20:14:10 -0300

    moved api to different repo

--- api/src/routers/favouritesRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const favouritesController = require('../controllers/favouritesController')
-
-router.post('/favourites/create-delete', favouritesController.createDelete)
-
-module.exports = router;
--- api/src/routers/genresRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const genresController = require('../controllers/genresController');
-
-router.get('/genres', genresController.list)
-
-module.exports = router
